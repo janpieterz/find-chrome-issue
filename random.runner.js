f09aa1c5cbc567222f7433672b05cf98153a1793
Jan-Pieter Zoutewelle <jp@intreba.nl>
2018-01-15 22:31:40 +0100

    Show case sensitivity appear

--- random.runner.js
+++ random.runner.js
@@ -1,9 +1,11 @@
 const { Chromeless } = require("chromeless");
-process.env.NODE_ENV = "test";
+process.env.FOO = "bar";
 async function run() {
   const chromeless = new Chromeless();
   const result = await chromeless.goto("https://www.google.com").screenshot();
   console.log(result);
+  console.log(process.env.FOO);
+  console.log(process.env.foo);
   await chromeless.end();
 }
 
--- random.test.js
+++ random.test.js
@@ -1,7 +1,9 @@
 const { Chromeless } = require("chromeless");
-
+process.env.FOO = "bar";
 it("google something", async () => {
   const chromeless = new Chromeless();
+  console.log(process.env.FOO);
+  console.log(process.env.foo);
   const result = await chromeless.goto("https://www.google.com").screenshot();
   expect(result).toBeDefined();
   await chromeless.end();
